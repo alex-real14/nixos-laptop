bd9b5b5394ac098c44e16d2b192a267351374283
Alex Real <alexander.real14@gmail.com>
2026-01-13 15:04:13 -0500

    commit

--- home/home.nix
+++ home/home.nix
@@ -20,14 +20,12 @@
 	};
 
 	xdg.configFile."hypr" = {
-		source = config.lib.file.mkOutOfStoreSymlink
-		"${config.home.homeDirectory}/.config/hypr";
+		source = config.lib.file.mkOutOfStoreSymlink "/home/alex/nixos/home/dotfiles/hypr";
 		recursive = true;
 	};
 
 	xdg.configFile."ghostty" = {
-		source = config.lib.file.mkOutOfStoreSymlink
-		"${config.home.homeDirectory}/.config/ghostty";
+		source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/dotfiles/ghostty";
 		recursive = true;
 	};
 
